fd10f47a884e46983517a8b9a5a69237ac88990b
kcsanjeeb <kcsanjeeb091@gmail.com>
2026-02-01 13:31:05 +0545

    removed plugin

--- medusa-plugin-wishlist/src/modules/wishlist/index.ts
+++
@@ -1,8 +0,0 @@
-import WishlistModuleService from "./service"
-import { Module } from "@medusajs/framework/utils"
-
-export const WISHLIST_MODULE = "wishlist"
-
-export default Module(WISHLIST_MODULE, {
-  service: WishlistModuleService,
-})
